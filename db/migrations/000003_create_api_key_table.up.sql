071ed477a83582b0eafe25a8f7f52b4944d4188b
Arne Molland <arne@molland.sh>
2024-10-31 21:10:25 +0100

    db, proto: update schemas, merge migrations

--- db/migrations/000003_create_api_key_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS api_key (
-    key_hash TEXT PRIMARY KEY,
-    device_id TEXT NOT NULL,
-    created_at TEXT NOT NULL DEFAULT (datetime('now'))
-);
-
-CREATE INDEX idx_api_key_device_id ON api_key(device_id);
--- db/migrations/000005_create_update_package_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS update_package;
-DROP INDEX IF EXISTS idx_update_package_release_date;
