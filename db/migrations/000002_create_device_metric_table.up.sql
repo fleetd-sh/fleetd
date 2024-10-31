db34b982305d79a27011f2db6306207170b9601d
Arne Molland <arne@molland.sh>
2024-10-31 21:10:25 +0100

    db, proto: update schemas, merge migrations

--- db/migrations/000002_create_device_metric_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS device_metric (
-    id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))),
-    device_id TEXT NOT NULL,
-    name TEXT NOT NULL,
-    value REAL NOT NULL,
-    timestamp TEXT NOT NULL DEFAULT (datetime('now')),
-    tags TEXT
-)
--- db/migrations/000005_create_update_package_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS update_package (
-    id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(16)))),
-    version TEXT NOT NULL,
-    release_date TEXT NOT NULL,
-    change_log TEXT,
-    file_url TEXT NOT NULL
-);
-
-CREATE INDEX IF NOT EXISTS idx_update_package_release_date ON update_package(release_date);
